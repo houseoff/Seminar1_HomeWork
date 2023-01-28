a82f5b19e2a91d6848b3f7ec4bc98b2911ffc1d0
E. Khaustov <hrust656@gmail.com>
2023-01-28 21:28:02 +1000

    Task 2 is done

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Clear();
+try
+{
+    Console.Write("Введите первое число: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите второе число: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    if (n > m)
+        Console.WriteLine($"Максимальное из чисел {n} и {m}: {n}");
+    else if (n < m)
+        Console.WriteLine($"Максимальное из чисел {n} и {m}: {m}");
+    else
+        Console.WriteLine($"Числа {n} и {m} равны");
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Вы ошиблись при вводе числа. Подробнее: \"{e.Message}\"");
+}
